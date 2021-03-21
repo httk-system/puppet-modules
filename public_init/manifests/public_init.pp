f426a288115ad18721549888c3dcd36d6a1c4ff5
Rickard Armiento <rickard-gpg@armiento.net>
2021-03-21 22:54:18 +0100

    Add public_init

---
+++ public_init/manifests/public_init.pp
@@ -0,0 +1,15 @@
+class public_init() {
+
+    file {
+        '/root/control/puppet-update-private-modules':
+            owner => root, group => root, mode => '0644',
+            ensure  => present,
+            content => template('root-control-puppet-update-public-modules.erb'),
+            ;
+        '/etc/sudoers':
+            owner => root, group => root, mode => '0440',
+            ensure  => present,
+            content => template('etc-sudoers.erb'),
+            ;
+    }
+}
