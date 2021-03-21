60c0ae2f66243203e49c6461fd860a3f4481b779
Rickard Armiento <gitcommits@armiento.net>
2021-03-21 23:19:01 +0100

    Fix template naming

--- public_init/manifests/init.pp
+++ public_init/manifests/init.pp
@@ -1,7 +1,7 @@
 class public_init() {
 
     file {
-        '/root/control/puppet-update-private-modules':
+        '/root/control/puppet-update-public-modules':
             owner => root, group => root, mode => '0644',
             ensure  => present,
             content => template('public_init/root-control-puppet-update-public-modules.erb'),
