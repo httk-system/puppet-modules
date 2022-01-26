8a448e6f304764ecc1a8d818dec5cb36a8ecd196
Rickard Armiento <gitcommits@armiento.net>
2022-01-26 02:00:52 +0100

    Fixes to certbot

--- certbot_apache/manifests/request.pp
+++ certbot_apache/manifests/request.pp
@@ -7,7 +7,7 @@ define certbot_apache::request (
   $notify = undef,
 ) {
 
-    $domain_args = join("-d",$certbot_domains,'-d')
+    $domain_args = $certbot_domains.join(' -d ')
 
     if !$agree_tos {
       fail("YOU MUST AGREE TO THE LETSENCRYPT TOS TO REQUEST CERTIFICATES (read tos and then set agree_tos=>true")
@@ -19,13 +19,20 @@ define certbot_apache::request (
       $eff_email_arg = "--no-eff-email"     
     }
 
+    if $https_redirect {
+      $redirect_arg = "--redirect"
+    } else {
+      $redirect_arg = "--no-redirect"     
+    }
+
     exec {
-	'certbot_update':
+	"certbot_request_$name":
 	    require => [
 			Package['certbot'],
 		        Package['python3-certbot-apache'],
 			],
-	    command => 'echo certbot --keep-until-expiring --apache -m $certbot_email $eff_email_arg $domain_args',
+	    command => "certbot --keep-until-expiring --apache -m $certbot_email $eff_email_arg $redirect_arg -d $domain_args",
+	    provider => shell,
             notify => $notify,
 	    logoutput => true,
     }
