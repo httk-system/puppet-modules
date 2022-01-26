eaa156d60ac14f5a24c1baa4f187b6efa28a05ca
Rickard Armiento <gitcommits@armiento.net>
2022-01-26 01:33:14 +0100

    Certbox fixes

--- certbot_apache/manifests/init.pp
+++ certbot_apache/manifests/init.pp
@@ -1,11 +1,5 @@
-class certbot_apache (
-  $certbot_domains,
-  $certbot_email,
-  $agree_tos,
-  $eff_email,
-  $https_redirect,
-  $notify = undef,
-) {
+class certbot_apache {
+
     include public_init
 
     package {
@@ -16,23 +10,4 @@ class certbot_apache (
             ensure => installed,
 	    ;
     }
-
-    file { '/root/bin/certbot_update.sh':
-	content => template('certbot_apache/root-bin-certbot_update.sh.erb'),
-	ensure => present,
-	owner => 'root', group => 'root', mode => '0700',
-    }
-
-    exec {
-	'/root/bin/certbot_update.sh':
-	    require => [
-			File['/root/bin/certbot_update.sh'],
-			File['/root/flags'],
-			Package['certbot'],
-		        Package['python3-certbot-apache'],
-			],
-            notify => $notify,
-	    logoutput => true,
-    }
-    
 }
