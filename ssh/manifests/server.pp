750a21e122729613297440a62c547523ddaa19ce
Rickard Armiento <gitcommit@armiento.net>
2022-03-08 11:25:27 +0100

    Integrate fail2ban with ssh

--- ssh/manifests/server.pp
+++ ssh/manifests/server.pp
@@ -1,7 +1,9 @@
 class ssh::server(
   $port = 22,
   $password_auth = false,
+  $fail2ban = true,
 ){
+
     package { 'ssh':
         ensure => present,
     } ->
@@ -25,4 +27,20 @@ class ssh::server(
 	ctstate => ['NEW','ESTABLISHED','RELATED'],
         action => 'accept',
     }
+
+    if $failtoban {
+
+      class { 'fail2ban':
+        package_ensure => 'latest',
+        email => 'root@localhost',
+        action => 'action_'
+      }
+
+      file {
+        '/root/control/fail2ban-unban':
+         content => template('ssh/root-control-fail2ban-unban.erb'),
+         ensure => present,
+         owner => 'root', group => 'root', mode => '0700',
+      }
+   }
 }
