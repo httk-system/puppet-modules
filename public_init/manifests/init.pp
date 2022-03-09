4c448e4d1e0269c2d1c90c299b89ce2368621d13
Rickard Armiento <rickard-gpg@armiento.net>
2022-03-09 17:15:35 +0100

    Adjust control script for puppet-update

--- public_init/manifests/init.pp
+++ public_init/manifests/init.pp
@@ -4,11 +4,11 @@ class public_init() {
     ensure_resource('file','/root/secrets',{ensure => 'directory',owner => 'root',mode => '0700'})
     ensure_resource('file','/root/flags',{ensure => 'directory',owner => 'root',mode => '0700'})
     ensure_resource('file','/root/bin',{ensure => 'directory',owner => 'root',mode => '0700'})
-    ensure_resource('file','/root/puppet',{ensure => 'directory',owner => 'root',mode => '0700'})    
+    ensure_resource('file','/root/puppet',{ensure => 'directory',owner => 'root',mode => '0700'})
 
     file {
         '/root/control/puppet-update-public-modules':
-            owner => root, group => root, mode => '0644',
+            owner => root, group => root, mode => '0755',
             ensure  => present,
 	    require => File['/root/control'],
             content => template('public_init/root-control-puppet-update-public-modules.erb'),
@@ -17,6 +17,6 @@ class public_init() {
             ensure  => 'link',
 	    require => File['/root/puppet'],
 	    target => '/etc/puppet/code/local-modules/public',
-            ;	    
+            ;
     }
 }
