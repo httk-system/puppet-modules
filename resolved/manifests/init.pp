cd87d1dd2fb124bce02335fbd37780caee84c4e9
Rickard Armiento <gitcommits@armiento.net>
2022-09-09 14:58:43 +0200

    systemd-resolved

---
+++ resolved/manifests/init.pp
@@ -0,0 +1,26 @@
+class resolved(
+   $ip,
+){
+
+    file { '/etc/systemd/resolved.conf':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => template( 'resolved/etc-systemd-resolved.conf.erb'),
+    } ~>
+    service { 'systemd-resolved':
+        ensure     => running,
+        hasstatus  => true,
+        hasrestart => true,
+        enable     => true,
+    }
+
+    firewall { '05 resolved':
+        dport   => "53",
+        proto  => ['udp','tcp'],
+	ctstate => ['NEW','ESTABLISHED','RELATED'],
+        action => 'accept',
+    }
+
+}
