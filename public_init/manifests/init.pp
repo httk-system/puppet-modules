8033187e5bbeb7aea39e6301a2cb6b3b4cca3e09
gitcommits@armiento.net <Rickard Armiento>
2022-09-22 09:29:45 +0000

    Reorganize control scripts

--- public_init/manifests/init.pp
+++ public_init/manifests/init.pp
@@ -1,23 +1,11 @@
 class public_init() {
 
-    ensure_resource('file','/root/control',{ensure => 'directory',owner => 'root',mode => '0700'})
+    ensure_resource('file','/usr/control',{ensure => 'directory',owner => 'root',mode => '0744'})
+    ensure_resource('file','/root/control',{ensure => 'link', target => '/usr/control'})
     ensure_resource('file','/root/secrets',{ensure => 'directory',owner => 'root',mode => '0700'})
     ensure_resource('file','/root/flags',{ensure => 'directory',owner => 'root',mode => '0700'})
     ensure_resource('file','/root/collectors',{ensure => 'directory',owner => 'root',mode => '0700'})
     ensure_resource('file','/root/bin',{ensure => 'directory',owner => 'root',mode => '0700'})
     ensure_resource('file','/root/puppet',{ensure => 'directory',owner => 'root',mode => '0700'})
 
-    file {
-        '/root/control/puppet-update-public-modules':
-            owner => root, group => root, mode => '0755',
-            ensure  => present,
-	    require => File['/root/control'],
-            content => template('public_init/root-control-puppet-update-public-modules.erb'),
-            ;
-        '/root/puppet/public-puppet-modules':
-            ensure  => 'link',
-	    require => File['/root/puppet'],
-	    target => '/etc/puppet/code/local-modules/public',
-            ;
-    }
 }
