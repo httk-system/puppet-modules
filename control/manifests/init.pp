02f54bce3f6ac1917e3ba9fdebf2ff73aaaf9777
Rickard Armiento <rickard-gpg@armiento.net>
2022-11-28 21:36:38 +0100

    control module

--- control/manifests/init.pp
+++ control/manifests/init.pp
@@ -6,4 +6,12 @@ class control() {
     mode => '0744',
   }
 
+  file { '/etc/profile.d/puppet-setups-control.sh':
+        ensure  => 'present',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        source  => 'puppet:///modules/control/etc-profile.d-puppet-setups-control.sh',
+  }
+  
 }
