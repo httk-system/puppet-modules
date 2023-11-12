fcd91c51fe689980b89917fb343b80be7292c74e
Rickard Armiento <rickard-gpg@armiento.net>
2023-11-12 12:14:08 +0000

    Fix to timesyncd

--- timesyncd/manifests/init.pp
+++ timesyncd/manifests/init.pp
@@ -1,13 +1,18 @@
 class timesyncd (
   $ntpservers
 ) {
-    file { '/etc/systemd/timesyncd.conf':
-	content => template('timesyncd/etc-systemd-timesyncd.conf.erb'),
-	ensure => present,
-	owner => 'root', group => 'root', mode => '0700',
-    } ~>
-    service { 'systemd-timesyncd':
-      ensure => 'running',
-      enable => true
-    }
+  
+  package { 'systemd-timesyncd':
+    ensure => 'installed'
+  }
+  ->
+  file { '/etc/systemd/timesyncd.conf':
+    content => template('timesyncd/etc-systemd-timesyncd.conf.erb'),
+    ensure => present,
+    owner => 'root', group => 'root', mode => '0700',
+  } ~>
+  service { 'systemd-timesyncd':
+    ensure => 'running',
+    enable => true
+  }
 }
