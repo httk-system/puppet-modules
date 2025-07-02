dad1d2b38379ec0163e8522193fce6b71fc602e6
Rickard Armiento <rickard-gpg@armiento.net>
2025-07-02 09:34:51 +0000

    Fix rights on timesyncd.conf

--- timesyncd/manifests/init.pp
+++ timesyncd/manifests/init.pp
@@ -9,7 +9,7 @@ class timesyncd (
   file { '/etc/systemd/timesyncd.conf':
     content => template('timesyncd/etc-systemd-timesyncd.conf.erb'),
     ensure => present,
-    owner => 'root', group => 'root', mode => '0744',
+    owner => 'root', group => 'root', mode => '0644',
   } ~>
   service { 'systemd-timesyncd':
     ensure => 'running',
